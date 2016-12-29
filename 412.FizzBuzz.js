94e9cade7393d6fc5f9053ce3bbf642096eae41d
KUO <drjkuo@gmail.com>
2016-12-29 12:46:28 +0800

    refactor

--- 412.FizzBuzz.js
+++ 412.FizzBuzz.js
@@ -3,30 +3,29 @@
  * @return {string[]}
  */
 var fizzBuzz = function(n) {
-    let str = [];
+    let str=[];
     let three = 3;
     let five = 5;
     for (let i=1; i<n+1; i=i+1)
     {
-        if ((i % (three*five)) === 0)
+        let tmp = "";
+        if (i % (three*five) === 0)
         {
             str.push("FizzBuzz");
-            continue;
         }
 
-        if ((i % three) === 0)
+        else if (i % (three) === 0)
         {
             str.push("Fizz");
-            continue;
         }
 
-        if (i % (five) === 0)
+        else if (i % (five) === 0)
         {
             str.push("Buzz");
-            continue;
         }
 
-        str.push(i.toString());
+        else str.push(i.toString());
+
     }
 
     return str;
