ee39fdebfdd42966152d86e2ec840f14ac81dec6
KUO <drjkuo@gmail.com>
2017-01-03 11:14:35 +0800

    faster solution; indexOf(); no need for set

--- 349.IntersectionOfTwoArrays.js
+++ 349.IntersectionOfTwoArrays.js
@@ -4,34 +4,14 @@
  * @return {number[]}
  */
 var intersection = function(nums1, nums2) {
-    let arr1 = nums1.slice();
     let l1 = nums1.length;
-
-    let arr2 = nums2.slice();
-    let l2 = nums2.length;
-
-    let noDupArr1 = new Set();
+    let ans = [];
     for (let i=0; i<l1; i++)
     {
-        noDupArr1.add(arr1[i]);
-    }
-
-    let noDupArr2 = new Set();
-    for (let i=0; i<l2; i++)
-    {
-        noDupArr2.add(arr2[i]);
-    }
-
-
-    let ans = [];
-    for (let y of noDupArr1)
-    {
-        for (let x of noDupArr2)
+        let tmp = nums1[i];
+        if (nums2.indexOf(tmp) >=0 && ans.indexOf(tmp) == -1)
         {
-            if (y === x)
-            {
-                ans.push(x);
-            }
+            ans.push(tmp);
         }
     }
 
