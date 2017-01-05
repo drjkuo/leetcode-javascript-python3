12323b331e38477211cd4b0614cd4dc54c07540a
KUO <drjkuo@gmail.com>
2017-01-06 03:42:23 +0800

    faster solution; 93%

--- 171.ExcelSheetColumnNumber.js
+++ 171.ExcelSheetColumnNumber.js
@@ -4,14 +4,28 @@
  */
 
 // Runtime: 122 ms
+// var titleToNumber = function(s) {
+//     // let arrS = s.split("");
+//     let sum = 0;
+//     let sl = s.length;
+//     for (let i=0; i<sl; i++)
+//     {
+//         sum += (s.charCodeAt((sl-1)-i)-64)*Math.pow(26,i); // return 65
+//         //chr = String.fromCharCode(65 + n);
+//     }
+
+//     return sum;
+
+// };
+
+// Runtime: 116 ms
 var titleToNumber = function(s) {
     // let arrS = s.split("");
     let sum = 0;
     let sl = s.length;
     for (let i=0; i<sl; i++)
     {
-        sum += (s.charCodeAt((sl-1)-i)-64)*Math.pow(26,i); // return 65
-        //chr = String.fromCharCode(65 + n);
+        sum = sum*26 + (s.charCodeAt(i)-64);
     }
 
     return sum;
