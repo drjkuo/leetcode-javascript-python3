b1ffb84e2c9cf6a9e64fb194025851e73f544ddc
KUO <drjkuo@gmail.com>
2017-01-06 04:01:09 +0800

    same speed

--- 242.ValidAnagram.js
+++ 242.ValidAnagram.js
@@ -3,7 +3,6 @@
  * @param {string} t
  * @return {boolean}
  */
-
 // Runtime: 119 ms
 var isAnagram = function(s, t) {
     let arr1 = s.split("");
@@ -17,11 +16,8 @@ var isAnagram = function(s, t) {
     for (let i=0; i<l1; i++)
     {
         tmp[s[i]] = tmp[s[i]] || 0;
+        tmp[t[i]] = tmp[t[i]] || 0;
         tmp[s[i]]++;
-    }
-
-    for (let i=0; i<l2; i++)
-    {
         tmp[t[i]]--;
     }
 
@@ -33,3 +29,35 @@ var isAnagram = function(s, t) {
     return true;
 
 };
+
+
+
+// // Runtime: 119 ms
+// var isAnagram = function(s, t) {
+//     let arr1 = s.split("");
+//     let l1 = s.length;
+//     let arr2 = t.split("");
+//     let l2 = t.length;
+//     let tmp = {};
+
+//     if (l1 !== l2) return false;
+
+//     for (let i=0; i<l1; i++)
+//     {
+//         tmp[s[i]] = tmp[s[i]] || 0;
+//         tmp[s[i]]++;
+//     }
+
+//     for (let i=0; i<l2; i++)
+//     {
+//         tmp[t[i]]--;
+//     }
+
+//     for (let x in tmp)
+//     {
+//         if (tmp[x] !== 0) return false;
+//     }
+
+//     return true;
+
+// };
