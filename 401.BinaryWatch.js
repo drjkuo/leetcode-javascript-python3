bca982687ccb18b630b4a4ddc899ff290be503c1
KUO <drjkuo@gmail.com>
2017-01-10 11:03:42 +0800

    dfs

--- 401.BinaryWatch.js
+++ 401.BinaryWatch.js
@@ -6,30 +6,61 @@
 // n -> 0000:000000 -> digit of hr:min
 // dfs
 
+// Runtime: 96 ms
+
 var readBinaryWatch = function(num) {
     var result = [];
     //var tmp = [];
-    let hr = [1,2,4,8]; // 0-3
-    let min = [1,2,4,8,16,32]; //4-9
-    dfs(result,[],0,9,num);
-    console.log(result);
+    dfs(result,[],0,11,num);
+    // console.log(result);
     return result;
 };
 
+var formatTime = function(h, m) {
+    if (m < 10) {
+        return `${h}:0${m}`
+    } else {
+        return `${h}:${m}`
+    }
+}
+
+let convertToTime = function(d)
+{
+    let hr=0;
+    let min=0;
+    let hrd = [1,2,4,8]; // 0-3
+    let mind = [1,2,4,8,16,32]; //4-9
+
+    for (let i=0; i<d.length; i++)
+    {
+        if (i<=3)
+        {
+            hr += d[i]*hrd[i];
+        }
+        else
+        {
+            min += d[i]*mind[i-4];
+        }
+    }
+
+    if (Number(hr)<=11 && Number(min)<=59) return formatTime(hr,min);
+}
+
 let dfs = function(result,tmp,clvl,tlvl,n)
 {
-    if (clvl === tlvl || n === 1)
+    if (clvl === tlvl) return;
+
+    if (n === 0)
     {
-        result.push(tmp);
+        if (convertToTime(tmp)) result.push(convertToTime(tmp));
         return;
     }
-    //let tmp = t;
+
     tmp.push(0);
-    dfs(result,tmp,clvl+1,tlvl,n-1);
+    dfs(result,tmp,clvl+1,tlvl,n);
     tmp.pop(0);
 
     tmp.push(1);
     dfs(result,tmp,clvl+1,tlvl,n-1);
     tmp.pop(1);
-    // for (let i=0; i<2; i++) dfs(result,clvl+1,tlvl)
 }
