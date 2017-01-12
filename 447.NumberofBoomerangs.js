1447d0aa9cfb95cf2fa4dd18f67f6ce681eceb88
KUO <drjkuo@gmail.com>
2017-01-12 21:20:51 +0800

    ?

---
+++ 447.NumberofBoomerangs.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var numberOfBoomerangs = function(points) {
+    let ans=0;
+    for (let i=0; i<points.length; i++)
+    {
+        let hashmap = {};
+        for (let j=0; j<points.length; j++)
+        {
+
+            let key = Math.pow(points[i][0]-points[j][0],2) + Math.pow(points[i][1]-points[j][1],2);
+            ans += (hashmap[key] || 0) * 2;
+            hashmap[key] = (hashmap[key] || 0) + 1;
+        }
+        // console.log(hashmap);
+    }
+    return ans;
+};
+
