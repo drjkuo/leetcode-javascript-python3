64bc45654f163f482481fcf0582e05cb8465d0cc
KUO <drjkuo@gmail.com>
2017-01-13 15:20:28 +0800

    Status: Memory Limit Exceeded

--- 447.NumberofBoomerangs.js
+++ 447.NumberofBoomerangs.js
@@ -11,9 +11,17 @@ var numberOfBoomerangs = function(points) {
         {
 
             let key = Math.pow(points[i][0]-points[j][0],2) + Math.pow(points[i][1]-points[j][1],2);
-            ans += (hashmap[key] || 0) * 2;
+            // ans += (hashmap[key] || 0) * 2;
             hashmap[key] = (hashmap[key] || 0) + 1;
         }
+
+        for (let x in hashmap)
+        {
+            ans += hashmap[x]*(hashmap[x]-1);
+        }
+        // hashmap = null;
+        // delete hashmap;
+
         // console.log(hashmap);
     }
     return ans;
