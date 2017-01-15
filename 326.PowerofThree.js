45f3c0abaa1a8333faf796100fc651cdaaef1fb7
KUO <drjkuo@gmail.com>
2017-01-15 21:47:55 +0800

    precision issue occurs when using log in js; use array to store all possible integers instead

--- 326.PowerofThree.js
+++ 326.PowerofThree.js
@@ -5,7 +5,7 @@
 var isPowerOfThree = function(n) {
     // log3(x)===int
 
-    let ans = Number.isInteger(Math.log(n)/Math.log(3));
-    console.log(Math.log(n)/Math.log(3));
-    return ans;
+    let arr = [1, 3, 9, 27, 81, 243, 729, 2187, 6561, 19683, 59049, 177147, 531441, 1594323, 4782969, 14348907, 43046721, 129140163, 387420489, 1162261467];
+    return (arr.indexOf(n) !== -1) ? true : false;
+    //return ans;
 };
