ec10f0b574eece63bbf90b08ea66b4bf5f1fa5f6
KUO <drjkuo@gmail.com>
2017-01-29 21:36:28 -0800

    manipulates digits

--- 66 Plus One.js
+++ 66 Plus One.js
@@ -2,6 +2,26 @@
  * @param {number[]} digits
  * @return {number[]}
  */
+// Runtime: 119 ms; 24%
+
 var plusOne = function(digits) {
-    return (parseInt(digits.join(""))+1).toString().split("").map(Number);
+    let carry = 0;
+    for (let i=digits.length-1; i>=0; i--)
+    {
+        digits[i] += carry;
+        carry = 0;
+
+        if (i===digits.length-1) digits[i]++;
+
+        if (digits[i] === 10)
+        {
+            digits[i] = 0;
+            carry = 1;
+        }
+    }
+
+    if (digits[0] === 0 && carry === 1) digits.unshift(1);
+
+    return digits;
+
 };
