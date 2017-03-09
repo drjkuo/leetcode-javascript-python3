6451808bd46f92c4d8b6118ee40c6a7d5c830b18
KUO <drjkuo@gmail.com>
2017-03-09 15:11:14 -0800

    recursion

--- 270. Closest Binary Search Tree Value.js
+++ 270. Closest Binary Search Tree Value.js
@@ -11,32 +11,19 @@
  * @return {number}
  */
 
-var minABS = Number.MAX_SAFE_INTEGER;
-var ans;
 
 var closestValue = function(root, target) {
-    dfs(root, target);
-    return ans;
+    var child = (root.val > target)? root.left : root.right;
+    if (child === null) return root.val;
+
+    var closesetInChild = closestValue(child, target);
+
+    return Math.abs(closesetInChild - target) < Math.abs(root.val - target)? closesetInChild : root.val;
+
+
 };
 
 
-var dfs = function(root, target) {
-    if (root === null) return;
 
-    if (root.val > target) {
-        if (Math.abs(root.val-target) < minABS) {
-            minABS = Math.abs(root.val-target);
-            ans = root.val;
-        }
-        dfs(root.left, target);
-    }
 
-    else { // root.val < target
-        if (Math.abs(root.val-target) < minABS) {
-            minABS = Math.abs(root.val-target);
-            ans = root.val;
-        }
-        dfs(root.right, target);
-    }
-};
 
