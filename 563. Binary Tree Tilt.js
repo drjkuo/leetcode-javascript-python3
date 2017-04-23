4c4b5c0339fb2f956f43cd9ec8d8378785afa2f3
KUO <drjkuo@gmail.com>
2017-04-22 20:53:59 -0700

    563

---
+++ 563. Binary Tree Tilt.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+
+var ans = 0;
+
+var findTilt = function(root) {
+    if (root === null) return 0
+
+    return Math.abs(sum(root.left) - sum(root.right)) + findTilt(root.left) + findTilt(root.right);
+    // return ans;
+
+};
+
+function sum(node) {
+    if (node === null) return 0;
+    // if (node.left === null && node.right === null) return node.val;
+    // if (node.right === null) return node.val + sum
+    return sum(node.right) + sum(node.left) + node.val;
+}
