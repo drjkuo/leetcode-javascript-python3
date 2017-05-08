6f5d5f96d41a0e96693494c603e55b2a8139817a
KUO <drjkuo@gmail.com>
2017-05-07 22:28:23 -0700

    BFS with the bug
    ["11111011111111101011",
     "01111111111110111110",
     "10111001101111111111",
     "11110111111111111111",
     "10011111111111111111",
     "10111111011101110111",
     "01111111111101101111",
     "11111111111101111011",
     "11111111110111111111",
     "11111111111111111111",
     "01111111011111111111",
     "11111111111111111111",
     "11111111111111111111",
     "11111011111110111111",
     "10111110111011110111",
     "11111111111101111110",
     "11111111111110111100",
     "11111111111111111111",
     "11111111111111111111",
     "11111111111111111111"]

--- 200. Number of Islands.js
+++ 200. Number of Islands.js
@@ -8,37 +8,57 @@ var numIslands = function(grid) {
     var rowlen = g.length;
     var collen = g[0].length;
     var result = 0;
+    var q = [];
     // console.log(typeof g[0][0]);
     for (var i=0; i<rowlen; i++) {
        for (var j=0; j<collen; j++) {
            if (g[i][j] === '1') {
                result++;
             //   console.log([i,j]);
-               DFS(i,j,g,rowlen,collen);
+            //   q.push([i,j]);
+               BFS(i,j,g,rowlen,collen,q);
            }
        }
     }
-
     return result;
 };
 
-function DFS(i,j,g,rowlen, collen) {
-    if (g[i][j] === '1') {
-        g[i][j] = g[i][j].replace('1', '0');
-        console.log(i,j);
-        console.log(g[i][j]);
-        // console.log(g[p[0]][p[1]]);
-        if ((i+1) <= rowlen-1) {
-            DFS(i+1,j,g,rowlen, collen);
-        }
-        if ((i-1) >= 0) {
-            DFS(i-1,j,g,rowlen, collen);
-        }
-        if ((j+1) <= collen-1) {
-            DFS(i,j+1,g,rowlen, collen);
-        }
-        if ((j-1) >= 0) {
-            DFS(i,j-1,g,rowlen, collen);
-        }
+function BFS(i,j,g,rowlen,collen,q) {
+    q.push([i,j]);
+    // g[i][j] = g[i][j].replace("1","0");
+    while (q.length > 0) {
+        var tmp = q.shift();
+        // console.log(tmp);
+        i = tmp[0];
+        j = tmp[1];
+        g[i][j] = g[i][j].replace("1","0");
+        if ((i+1)<=rowlen-1 && g[i+1][j] === '1') q.push([i+1, j]);
+        if ((i-1)>=0 && g[i-1][j] === '1') q.push([i-1, j]);
+        if ((j+1)<=collen-1 && g[i][j+1] === '1') q.push([i, j+1]);
+        if ((j-1)>=0 && g[i][j-1] === '1') q.push([i, j-1]);
     }
+    return;
+
 }
+
+
+// function DFS(i,j,g,rowlen, collen) {
+//     if (g[i][j] === '1') {
+//         g[i][j] = g[i][j].replace('1', '0');
+//         console.log(i,j);
+//         console.log(g[i][j]);
+//         // console.log(g[p[0]][p[1]]);
+//         if ((i+1) <= rowlen-1) {
+//             DFS(i+1,j,g,rowlen, collen);
+//         }
+//         if ((i-1) >= 0) {
+//             DFS(i-1,j,g,rowlen, collen);
+//         }
+//         if ((j+1) <= collen-1) {
+//             DFS(i,j+1,g,rowlen, collen);
+//         }
+//         if ((j-1) >= 0) {
+//             DFS(i,j-1,g,rowlen, collen);
+//         }
+//     }
+// }
