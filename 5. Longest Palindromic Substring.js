9c885bc4d42ca3ad2dbb02b85fc8639b5ce3c8b0
KUO <drjkuo@gmail.com>
2017-07-09 17:17:54 -0700

    updated -- bug free

--- 5. Longest Palindromic Substring.js
+++ 5. Longest Palindromic Substring.js
@@ -3,35 +3,35 @@
  * @return {string}
  */
 
-var ans="";
+
 
 var longestPalindrome = function(s) {
+    var ans="";
+
+    if(s === null || s.length === 0){
+        return "";
+    }
+
 
     for (var i = 0; i < s.length; i++) {
         onePalindrome (s, i, i);
-        console.log('single',ans);
-    }
-
-    for (var i = 0; i < s.length - 1; i++) {
-        if (s[i] === s[i+1]) {
-            onePalindrome (s, i, i+1);
-            console.log(ans);
-        }
+        onePalindrome (s, i, i+1);
+        // console.log('single',ans);
     }
 
     return ans;
+
+    function onePalindrome (s, start, end) {
+        while (start >=0 && end < s.length && s[start] === s[end]) {
+            start--;
+            end++;
+        }
+
+        var cur = s.substring(start+1, end);
+
+        if (cur.length > ans.length) {
+            ans = cur.slice();
+        }
+
+    }
 };
-
-function onePalindrome (s, start, end) {
-    while (start >=0 && end < s.length && s[start] === s[end]) {
-        start--;
-        end++;
-    }
-
-    var cur = s.substring(start+1, end);
-
-    if (cur.length > ans.length) {
-        ans = cur.slice();
-    }
-
-}
