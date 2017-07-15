4b62040dbdd982d110acf5de5e5ed6aff804dc5b
KUO <drjkuo@gmail.com>
2017-07-15 16:30:53 -0700

    Time Limit Exceeded

--- 139. Word Break.js
+++ 139. Word Break.js
@@ -5,8 +5,10 @@
  */
 var wordBreak = function(s, wordDict) {
     var results = [];
-    helper(wordDict, [], results);
+    if (wordDict.length === 0) return false;
+    // if (helper(wordDict, "", s)) return true;
 
+    helper(wordDict, "", s, results);
     for (var i=0; i<results.length; i++) {
         if (results[i] === s)
             return true;
@@ -15,15 +17,24 @@ var wordBreak = function(s, wordDict) {
     return false;
 };
 
-function helper(wordDict, oneAns, results) {
-    if (oneAns.length === wordDict.length) {
-        results.push(oneAns.join(""));
+function helper(wordDict, oneAns, s, results) {
+    // if oneAns.join("") ===
+    // console.log(typeof oneAns);
+    if (oneAns.length === s.length) {
+        results.push(oneAns);
         return;
     }
+
+    if (oneAns.length > s.length) {
+        return;
+    }
+
     for (var i=0; i<wordDict.length; i++) {
-        var tmpWordDict = wordDict.slice(); //.splice(i,1);
-        var tmpAns = oneAns.slice();
-        tmpAns.push(wordDict[i]);
-        helper(tmpWordDict,  tmpAns, results);
+        // var tmpWordDict = wordDict.slice(); //.splice(i,1);
+        // var tmpAns = oneAns.slice();
+        var tmpAns = oneAns + wordDict[i];
+        // console.log(tmpAns);
+        helper(wordDict, tmpAns, s, results);
+
     }
 }
