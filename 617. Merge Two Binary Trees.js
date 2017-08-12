85a0aa34023de68105046bf4e719d13159e15bad
KUO <drjkuo@gmail.com>
2017-08-12 13:07:28 -0700

    Good

--- 617. Merge Two Binary Trees.js
+++ 617. Merge Two Binary Trees.js
@@ -11,53 +11,15 @@
  * @return {TreeNode}
  */
 var mergeTrees = function(t1, t2) {
-    if (t1 === null && t2 === null) {
-        return null;
+    if (t1 === null) {
+        return t2;
     }
-    return DFS(t1, t2);
+    if (t2 === null) {
+        return t1;
+    }
+    // t1, t2 are not null
+    t1.val += t2.val;
+    t1.left = mergeTrees(t1.left, t2.left);
+    t1.right = mergeTrees(t1.right, t2.right);
+    return t1;
 };
-
-function DFS(t1, t2) {
-    var node = createNode(t1,t2);
-    // console.log(node);
-    if (t1 === null && t2 === null) {
-        return node;
-    }
-
-    if (t1 && t2) {
-        node.left = DFS(t1.left, t2.left);
-        node.right = DFS(t1.right, t2.right);
-        return node;
-    }
-    if (t1) {
-        node.left = DFS(t1.left, null);
-        node.right = DFS(t1.right, null);
-        return node;
-    }
-
-    if (t2) {
-        node.left = DFS(null, t2.left);
-        node.right = DFS(null, t2.right);
-        return node;
-    }
-
-}
-
-function createNode(t1, t2) {
-    if (t1 === null && t2 === null) {
-        return null;
-    }
-
-    if (t1 && t2) {
-        return new TreeNode(t1.val+t2.val);
-    }
-
-    if (t1) {
-        return new TreeNode(t1.val);
-    }
-
-    if (t2) {
-        return new TreeNode(t2.val);
-    }
-
-}
