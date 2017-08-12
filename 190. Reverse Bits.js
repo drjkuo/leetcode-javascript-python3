ad4d62acbd252f5cb26ee7f18f0c59977c990b41
KUO <drjkuo@gmail.com>
2017-08-12 02:30:08 -0700

    another

--- 190. Reverse Bits.js
+++ 190. Reverse Bits.js
@@ -7,15 +7,19 @@ var reverseBits = function(n) {
     var ans = 0;
     for (var i=0; i<32; i++) {
         ans = ans << 1 | (n&1);
+        // console.log(ans);
         n = n >> 1;
     }
 
     // signed to unsigned
     var fans = 0;
-    for (i=0; i<32; i++) {
-        var tmp = (ans&1)? Math.pow(2,i) : 0;
-        fans += tmp;
-        ans = ans >> 1;
-    }
+    // for (i=0; i<32; i++) {
+    //     var tmp = (ans&1)? Math.pow(2,i) : 0;
+    //     fans += tmp;
+    //     ans = ans >> 1;
+    // }
+    var all1 = -1;
+    var msb1 = -Math.pow(2,31);
+    fans = (ans & all1) + ((ans&msb1)? Math.pow(2,32) : 0);
     return (fans);
 };
