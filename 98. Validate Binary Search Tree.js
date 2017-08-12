be77342544bd3c61ef66b88b4cfc5b8fd17148f5
KUO <drjkuo@gmail.com>
2017-08-12 16:28:17 -0700

    in-order traverse BST produces an ascending sequence

--- 98. Validate Binary Search Tree.js
+++ 98. Validate Binary Search Tree.js
@@ -1,3 +1,14 @@
+// /**
+//  * Definition for a binary tree node.
+//  * function TreeNode(val) {
+//  *     this.val = val;
+//  *     this.left = this.right = null;
+//  * }
+//  */
+// /**
+//  * @param {TreeNode} root
+//  * @return {boolean}
+//  */
 /**
  * Definition for a binary tree node.
  * function TreeNode(val) {
@@ -10,31 +21,51 @@
  * @return {boolean}
  */
 var isValidBST = function(root) {
-    if (!root) return true;
-
-    var leftMax = helper(root.left)[0];
-    var rightMin = helper(root.right)[1];
-
-    if (root.val > leftMax && root.val < rightMin) {
-        return isValidBST(root.left) && isValidBST(root.right);
+    // in-order traversal
+    if (root === null) return true;
+    var all = [];
+    DFS(root);
+    for (var i=1; i<all.length; i++) {
+        if (all[i] <= all[i-1])
+            return false;
     }
-    else {
-        return false;
+    return true;
+
+    function DFS(root) {
+        if (root) {
+            DFS(root.left);
+            all.push(root.val);
+            DFS(root.right);
+        }
     }
 };
 
-function helper(node) {
-    var max = Number.MIN_SAFE_INTEGER;
-    var min = Number.MAX_SAFE_INTEGER;
-    DFS(node);
-    return [max, min];
-
-    function DFS(node) {
-        if (node) {
-            if (node.val > max) max = node.val;
-            if (node.val < min) min = node.val;
-            DFS(node.left);
-            DFS(node.right);
-        }
-    }
-}
+// var isValidBST = function(root) {
+//     if (!root) return true;
+//
+//     var leftMax = helper(root.left)[0];
+//     var rightMin = helper(root.right)[1];
+//
+//     if (root.val > leftMax && root.val < rightMin) {
+//         return isValidBST(root.left) && isValidBST(root.right);
+//     }
+//     else {
+//         return false;
+//     }
+// };
+//
+// function helper(node) {
+//     var max = Number.MIN_SAFE_INTEGER;
+//     var min = Number.MAX_SAFE_INTEGER;
+//     DFS(node);
+//     return [max, min];
+//
+//     function DFS(node) {
+//         if (node) {
+//             if (node.val > max) max = node.val;
+//             if (node.val < min) min = node.val;
+//             DFS(node.left);
+//             DFS(node.right);
+//         }
+//     }
+// }
