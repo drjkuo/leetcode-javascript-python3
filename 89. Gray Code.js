22b70d4afea3f6d671b99b5d4e36d3c4637e0f6e
KUO <drjkuo@gmail.com>
2017-08-13 14:50:42 -0700

    Much better, Math, slice()

--- 89. Gray Code.js
+++ 89. Gray Code.js
@@ -3,16 +3,15 @@
  * @return {number[]}
  */
 var grayCode = function(n) {
-    var ans = ["0", "1"]; // 2nd ["11", "10"]
-    for (var i=1; i<n; i++) {
-        var toBePush = ans.slice().reverse().map(function(x) {
-           return String(1).concat(x);
-        });
-        ans.push(...toBePush);
+    var ans = [0];
 
-        console.log(ans);
+    for (var i=0; i<n; i++) {
+        var toBePush = ans.slice().reverse().map(function(x) {
+            return x + Math.pow(2,i);
+        })
+        ans.push(...toBePush);
     }
-    return ans.map(function(x) {
-        parseInt(x,2)
-    });
+
+    return ans;
+
 };
