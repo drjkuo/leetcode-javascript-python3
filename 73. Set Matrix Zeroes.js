0283256ae304cf799e61c20cae7c6a21fc22e486
KUO <drjkuo@gmail.com>
2017-08-16 00:23:39 -0700

    in-place, save 1st row and 1st col results, and then use them to store temp info

--- 73. Set Matrix Zeroes.js
+++ 73. Set Matrix Zeroes.js
@@ -3,21 +3,50 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var setZeroes = function(matrix) {
-    var row = [];
-    var col = [];
-    for (var i=0; i<matrix.length; i++) {
-        for (var j=0; j<matrix[0].length; j++) {
-            if (matrix[i][j] === 0) {
-                row.push(i);
-                col.push(j);
-            }
+    // check 1st row
+    var firstRowZero = false;
+    for (var i=0; i<matrix[0].length; i++) {
+        if (matrix[0][i] === 0) {
+            firstRowZero = true;
+            break;
+        }
+    }
+
+    // check 1st col
+    var firstColZero = false;
+    for (i=0; i<matrix.length; i++) {
+        if (matrix[i][0] === 0) {
+            firstColZero = true;
+            break;
         }
     }
 
     for (var i=0; i<matrix.length; i++) {
         for (var j=0; j<matrix[0].length; j++) {
-            if ((row.indexOf(i) > -1) || (col.indexOf(j) > -1))
+            if (matrix[i][j] === 0) {
+                // matrix[i][j]=1;
+                matrix[i][0]=0;
+                matrix[0][j]=0;
+            }
+        }
+    }
+
+    for (var i=1; i<matrix.length; i++) {
+        for (var j=1; j<matrix[0].length; j++) {
+            if (matrix[i][0]===0 || matrix[0][j]===0)
                 matrix[i][j] = 0;
+        }
+    }
+
+    if (firstRowZero === true) {
+        for (var i=0; i<matrix[0].length; i++) {
+            matrix[0][i] = 0;
+        }
+    }
+
+    if (firstColZero === true) {
+        for (var i=0; i<matrix.length; i++) {
+            matrix[i][0] = 0;
         }
     }
 
