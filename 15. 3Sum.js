6940bc0e9a44391971f107885f354fc512305656
KUO <drjkuo@gmail.com>
2017-08-16 16:34:02 -0700

    cleaner answer

--- 15. 3Sum.js
+++ 15. 3Sum.js
@@ -2,57 +2,37 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var twoSumResult = function(nums) {
-    var result = {};
-    var tmp;
-    for(var i=0; i<nums.length; i++) {
-        for(var j=i+1; j<nums.length; j++) {
-            tmp = nums[i] + nums[j];
-            result[tmp] = result[tmp] || [];
-            result[tmp].push([nums[i],nums[j]]);
-        }
-    }
-    return result;
-}
-
-var diff = function(i,j,k) {
-    if (i === j) {
-        return false;
-    }
-    if (k === j) {
-        return false;
-    }
-    if (i === k) {
-        return false;
-    }
-
-    return true;
-}
 
 var threeSum = function(nums) {
-    nums = nums.sort(cmp);
+    // sort in ascending order
+    nums.sort(function(a,b) {
+       return a-b;
+    });
+
     var ans = [];
-    var twoSum = twoSumResult(nums);
-    console.log(twoSum);
-    for (var i=0; i<nums.length; i++) {
-        for (var j in twoSum) {
-            console.log(j);
-            if ((parseInt(j) + nums[i]) === 0) {
-                for (var k=0; k<twoSum[j].length; k++) {
-                    ans.push([twoSum[j][k][0], twoSum[j][k][1], nums[i]])
-                }
-            }
+    // shift 1st number, and check whether its right-hand numbers produces a corresponding two sum
+    for (var i=0; i<nums.length-2; i++) {
+        var tmp = twoSum(-nums[i], i+1, nums.length-1);
+        if (tmp !== false) {
+            ans.push([nums[i], nums[tmp[0]], nums[tmp[1]]]);
         }
     }
-    console.log(ans);
     return ans;
-};
 
-function cmp(a,b) {
-    if (a<b) {
-        return -1;
+
+    // two sum helper func
+    function twoSum (target, start, end) {
+        while (start !== end) {
+            if ((nums[start]+nums[end]) > target) {
+                end--;
+            }
+            else if ((nums[start]+nums[end]) < target) {
+                start++;
+            }
+            else {
+                return [start, end];
+            }
+        }
+        return false;
     }
-    else {
-        return 1;
-    }
-}
+};
