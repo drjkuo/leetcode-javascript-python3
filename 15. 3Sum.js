be2215b9303dcc18290396e05dadadc9be844e6f
KUO <drjkuo@gmail.com>
2017-08-16 18:26:24 -0700

    Time Limit Exceeded

--- 15. 3Sum.js
+++ 15. 3Sum.js
@@ -13,8 +13,11 @@ var threeSum = function(nums) {
     // shift 1st number, and check whether its right-hand numbers produces a corresponding two sum
     for (var i=0; i<nums.length-2; i++) {
         var tmp = twoSum(-nums[i], i+1, nums.length-1);
-        if (tmp !== false) {
-            ans.push([nums[i], nums[tmp[0]], nums[tmp[1]]]);
+        // console.log(tmp);
+        var tmpLen = tmp.length;
+        if (tmpLen === 0) continue;
+        for (var j=0; j<tmpLen; j++) {
+            ans.push([ nums[i], nums[tmp[j][0]], nums[tmp[j][1]] ]);
         }
     }
 
@@ -36,6 +39,7 @@ var threeSum = function(nums) {
 
     // two sum helper func
     function twoSum (target, start, end) {
+        var ans3 = [];
         while (start !== end) {
             if ((nums[start]+nums[end]) > target) {
                 end--;
@@ -44,9 +48,10 @@ var threeSum = function(nums) {
                 start++;
             }
             else {
-                return [start, end];
+                ans3.push([start, end]);
+                start++;
             }
         }
-        return false;
+        return ans3;
     }
 };
