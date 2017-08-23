67c5c657470c1a6cce9ef6810ee9e94957e64bd3
KUO <drjkuo@gmail.com>
2017-08-23 02:45:23 -0700

    simple hash

---
+++ 1. Two Sum.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    var hash = {};
+    for (var i=0; i<nums.length; i++) {
+        hash[nums[i]] = i;
+    }
+
+    for (i=0; i<nums.length; i++) {
+        if (hash[target-nums[i]] > 0) {
+            return [i, hash[target-nums[i]]];
+        }
+    }
+
+};
