7ae17929168babc7e0f148b854b9d3849f96993f
KUO <drjkuo@gmail.com>
2017-09-05 00:27:03 +0800

    hashmap

--- 242.ValidAnagram.js
+++ 242.ValidAnagram.js
@@ -19,3 +19,29 @@ var isAnagram = function(s, t) {
 
 };
 
+// hashmap
+var isAnagram = function(s, t) {
+    var l1 = s.length;
+    var l2 = t.length;
+    var h1 = {};
+
+    if (l1 !== l2) return false;
+
+    for (var i=0; i<l1; i++) {
+        h1[s[i]] = h1[s[i]] || 0;
+        h1[s[i]]++;
+    }
+
+    for (i=0; i<l2; i++) {
+        h1[t[i]] = h1[t[i]] || 0;
+        h1[t[i]]--;
+    }
+
+    for (i in h1) {
+        if (h1[i] !== 0) {
+            return false;
+        }
+    }
+
+    return true;
+};
