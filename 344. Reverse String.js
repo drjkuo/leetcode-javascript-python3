0bd606bc559cb1de6a8165b499558865ccb00b23
KUO <drjkuo@gmail.com>
2017-12-14 01:23:07 -0800

    reverse()

--- 344. Reverse String.js
+++ 344. Reverse String.js
@@ -3,15 +3,5 @@
  * @return {string}
  */
 var reverseString = function(s) {
-    var arr = s.split("");
-    var start = 0, end = s.length - 1, tmp;
-    while (start < end) {
-        tmp = arr[start];
-        arr[start] = arr[end];
-        arr[end] = tmp;
-        start++;
-        end--;
-    }
-
-    return arr.join("");
+    return s.split("").reverse().join("");
 };
