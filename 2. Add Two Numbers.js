b7d76a0fb355bebe71ef35d53b0c2c9d49865f1e
KUO <drjkuo@gmail.com>
2018-01-01 03:30:15 -0800

    run node by node

--- 2. Add Two Numbers.js
+++ 2. Add Two Numbers.js
@@ -11,27 +11,34 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
-    var sum = addLinkedList(l1) + addLinkedList(l2);
+    if (l1 === null || l2 === null) {
+        return l1 || l2;
+    }
 
     var ans = new ListNode(0);
     var tmp = ans;
-    if (sum === 0) return ans;
-    while (sum !== 0) {
-        var node = new ListNode(sum%10);
-        sum = Math.floor(sum/10);
-        tmp.next = node;
+    var carry = 0;
+    var digit = 0;
+
+    while (l1 || l2 || carry !== 0) {
+        digit = carry;
+        if (l1) {
+            digit += l1.val;
+            l1 = l1.next;
+        }
+        if (l2) {
+            digit += l2.val;
+            l2 = l2.next;
+        }
+
+        carry = Math.floor(digit/10);
+        digit = digit % 10;
+
+
+        tmp.next = new ListNode(digit);
         tmp = tmp.next;
     }
+
     return ans.next;
 
-    function addLinkedList(l) {
-        var sum = 0;
-        var exp = 0;
-        while (l) {
-            sum += l.val * Math.pow(10, exp);
-            l = l.next;
-            exp++;
-        }
-        return sum;
-    }
 };
