b1358681f15ac1604b2eb721ffb3ac7979e21a72
KUO <drjkuo@gmail.com>
2018-01-01 02:01:05 -0800

    OK

--- 53. Maximum Subarray.js
+++ 53. Maximum Subarray.js
@@ -6,14 +6,10 @@ var maxSubArray = function(nums) {
     // f[i] = (f[i-1] > 0) ? f[i-1] + nums[i] : nums[i]
     if (nums.length === 0) return 0;
     var f = [], ans = -Infinity;
-
-    for (var i=0; i<nums.length; i++) {
-        if (i===0) {
-            f[0] = nums[0];
-        }
-        else {
-            f[i] = (f[i-1] > 0)? f[i-1]+nums[i] : nums[i];
-        }
+    f[0] = nums[0];
+    ans = nums[0];
+    for (var i=1; i<nums.length; i++) {
+        f[i] = (f[i-1] > 0)? f[i-1]+nums[i] : nums[i];
         ans = Math.max(ans, f[i]);
     }
     return ans;
