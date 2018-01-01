fcbecadc1a9b5400200d99015c6303e5232c1acc
KUO <drjkuo@gmail.com>
2018-01-01 06:38:37 -0800

    OK

--- 20. Valid Parentheses.js
+++ 20. Valid Parentheses.js
@@ -2,25 +2,20 @@
  * @param {string} s
  * @return {boolean}
  */
-
- //92%
 var isValid = function(s) {
-    let tmp = [];
-    for (let i=0; i<s.length; i++)
-    {
-        if (s[i] === '(' || s[i] === '{' || s[i] === '[')
-        {
-            tmp.push(s[i]);
+    var stack = [];
+    for (var i=0; i<s.length; i++) {
+        if (s[i] === "(" || s[i] === "[" || s[i] === "{") {
+            stack.push(s[i])
         }
-        else // (s[i] === ')' || s[i] === '}' || s[i] === ']'):
-        {
-            if (s[i] === ')' && tmp.pop() !== '(') return false;
-            if (s[i] === '}' && tmp.pop() !== '{') return false;
-            if (s[i] === ']' && tmp.pop() !== '[') return false;
+        else {
+            var tmp = stack.pop();
+            if (tmp === "(" && s[i] === ")") continue;
+            if (tmp === "[" && s[i] === "]") continue;
+            if (tmp === "{" && s[i] === "}") continue;
+            return false;
         }
     }
-
-    if (tmp.length > 0) return false;
-
+    if (stack.length > 0) return false;
     return true;
 };
