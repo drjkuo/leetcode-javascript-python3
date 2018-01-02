602d3d76acc01c765a6411b98e828a3db55ad853
KUO <drjkuo@gmail.com>
2018-01-01 16:42:10 -0800

    add Java sol

--- 5. Longest Palindromic Substring.js
+++ 5. Longest Palindromic Substring.js
@@ -35,3 +35,38 @@ var longestPalindrome = function(s) {
 
     }
 };
+
+/*
+Java sol
+
+class Solution {
+    public int gstart, gend, maxlen = 0;
+
+    public String longestPalindrome(String s) {
+        for (int i=0; i<s.length(); i++) {
+            System.out.print(s.charAt(i)+" ");
+            checkPalindrome(s, i, i);
+            checkPalindrome(s, i, i+1);
+        }
+        return s.substring(gstart+1, gend);
+
+    }
+
+    public void checkPalindrome (String s, int start, int end) {
+        while (start >= 0 && end < s.length() && s.charAt(start) == s.charAt(end)) {
+            start--;
+            end++;
+        }
+
+        int len = end - start - 1;
+        if (len > maxlen) {
+            gstart = start;
+            gend = end;
+            maxlen = len;
+        }
+    }
+}
+
+
+
+*/
