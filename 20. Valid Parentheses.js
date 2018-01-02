49842e12cdd4be9390c54e87692ae127192614c5
KUO <drjkuo@gmail.com>
2018-01-01 17:42:44 -0800

    Java sol

--- 20. Valid Parentheses.js
+++ 20. Valid Parentheses.js
@@ -6,7 +6,7 @@ var isValid = function(s) {
     var stack = [];
     for (var i=0; i<s.length; i++) {
         if (s[i] === "(" || s[i] === "[" || s[i] === "{") {
-            stack.push(s[i])
+            stack.push(s[i]);
         }
         else {
             var tmp = stack.pop();
@@ -19,3 +19,32 @@ var isValid = function(s) {
     if (stack.length > 0) return false;
     return true;
 };
+
+/*
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack;
+        stack = new Stack<Character>();
+        for (char c : s.toCharArray()) {
+            // System.out.print((int)(c));
+            if (c == '(' || c == '[' || c == '{' )
+            {
+                stack.push(c);
+            }
+            else {
+                if (stack.size() == 0) return false;
+
+                Character tmp = stack.pop();
+                if (tmp == '(' && c== ')') continue;
+                if (tmp == '[' && c == ']') continue;
+                if (tmp == '{' && c == '}') continue;
+                return false;
+            }
+        }
+
+        return (stack.size() == 0);
+    }
+}
+
+
+*/
