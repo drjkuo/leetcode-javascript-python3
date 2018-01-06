69ec8b2cabd12cff63e5a4d6d66e5cc5fed86e1f
KUO <drjkuo@gmail.com>
2018-01-06 01:01:22 -0800

    this

--- 155. Min Stack.js
+++ 155. Min Stack.js
@@ -1,49 +1,113 @@
-class MinStack {
+/**
+ * initialize your data structure here.
+ */
+var MinStack = function() {
+    this.smain = [];
+    this.smin = [];
+};
 
-    Stack<Integer> smain;
-    Stack<Integer> smin;
-    /** initialize your data structure here. */
-    public MinStack() {
-        smain=new Stack<>();
-        smin=new Stack<>();
+/**
+ * @param {number} x
+ * @return {void}
+ */
+MinStack.prototype.push = function(x) {
+    var smain = this.smain;
+    var smin = this.smin;
+    smain.push(x);
+    if (smin.length === 0) {
+        smin.push(x);
     }
+    else if (x < smin[smin.length-1]) {
+        smin.push(x);
+    }
+    else {
+        smin.push(smin[smin.length-1]);
+    }
+};
 
-    public void push(int x) {
-        smain.push(x);
-        if (smin.size() == 0) {
-            smin.push(x);
-        }
-        else if (x < smin.peek()) {
-            smin.push(x);
-        }
-        else {
-            smin.push(smin.peek());
-        }
-        return;
-    }
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    var smain = this.smain;
+    var smin = this.smin;
+    smain.pop();
+    smin.pop();
+};
 
-    public void pop() {
-        if (smain.size() > 0) {
-            smain.pop();
-            smin.pop();
-            return;
-        }
-    }
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    var smain = this.smain;
+    // var smin = this.smin;
+    return smain[smain.length-1];
+};
 
-    public int top() {
-        return smain.peek();
-    }
-
-    public int getMin() {
-        return smin.peek();
-    }
-}
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+        // var smain = this.smain;
+    var smin = this.smin;
+    return smin[smin.length-1];
+};
 
 /**
  * Your MinStack object will be instantiated and called as such:
- * MinStack obj = new MinStack();
- * obj.push(x);
- * obj.pop();
- * int param_3 = obj.top();
- * int param_4 = obj.getMin();
+ * var obj = Object.create(MinStack).createNew()
+ * obj.push(x)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
  */
+
+// class MinStack {
+//
+//     Stack<Integer> smain;
+//     Stack<Integer> smin;
+//     /** initialize your data structure here. */
+//     public MinStack() {
+//         smain=new Stack<>();
+//         smin=new Stack<>();
+//     }
+//
+//     public void push(int x) {
+//         smain.push(x);
+//         if (smin.size() == 0) {
+//             smin.push(x);
+//         }
+//         else if (x < smin.peek()) {
+//             smin.push(x);
+//         }
+//         else {
+//             smin.push(smin.peek());
+//         }
+//         return;
+//     }
+//
+//     public void pop() {
+//         if (smain.size() > 0) {
+//             smain.pop();
+//             smin.pop();
+//             return;
+//         }
+//     }
+//
+//     public int top() {
+//         return smain.peek();
+//     }
+//
+//     public int getMin() {
+//         return smin.peek();
+//     }
+// }
+//
+// /**
+//  * Your MinStack object will be instantiated and called as such:
+//  * MinStack obj = new MinStack();
+//  * obj.push(x);
+//  * obj.pop();
+//  * int param_3 = obj.top();
+//  * int param_4 = obj.getMin();
+//  */
