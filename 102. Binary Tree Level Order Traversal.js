d357ac1d0bd5c50adbb31ec0ac53e548e5015d84
KUO <drjkuo@gmail.com>
2018-01-21 18:19:33 -0800

    double queue

--- 102. Binary Tree Level Order Traversal.js
+++ 102. Binary Tree Level Order Traversal.js
@@ -41,3 +41,38 @@ var levelOrder = function(root) {
     return ans;
 
 };
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+    var result = [];
+    var q = [], helperQ = [];
+    if (root === null) return [];
+    q.push(root);
+
+    var oneResult = [];
+    while(q.length !== 0 || helperQ.length !== 0) {
+        while (q.length !== 0) {
+            var tmp = q.shift();
+            oneResult.push(tmp.val);
+            if (tmp.left) helperQ.push(tmp.left);
+            if (tmp.right) helperQ.push(tmp.right);
+        }
+        q = helperQ;
+        helperQ = [];
+        result.push(oneResult.slice());
+        oneResult = [];
+    }
+
+    return result;
+};
