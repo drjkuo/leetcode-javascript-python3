94023b50e964ef7c799b1bd12164b193a883663c
KUO <drjkuo@gmail.com>
2018-01-21 16:35:17 -0800

    recursive

--- 98. Validate Binary Search Tree.js
+++ 98. Validate Binary Search Tree.js
@@ -40,32 +40,27 @@ var isValidBST = function(root) {
     }
 };
 
-// var isValidBST = function(root) {
-//     if (!root) return true;
-//
-//     var leftMax = helper(root.left)[0];
-//     var rightMin = helper(root.right)[1];
-//
-//     if (root.val > leftMax && root.val < rightMin) {
-//         return isValidBST(root.left) && isValidBST(root.right);
-//     }
-//     else {
-//         return false;
-//     }
-// };
-//
-// function helper(node) {
-//     var max = Number.MIN_SAFE_INTEGER;
-//     var min = Number.MAX_SAFE_INTEGER;
-//     DFS(node);
-//     return [max, min];
-//
-//     function DFS(node) {
-//         if (node) {
-//             if (node.val > max) max = node.val;
-//             if (node.val < min) min = node.val;
-//             DFS(node.left);
-//             DFS(node.right);
-//         }
-//     }
-// }
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root) {
+    return helper(root, Infinity, -Infinity);
+};
+
+function helper(node, max, min) {
+    if (node === null) return true;
+
+    if (node.val >= max) return false;
+    if (node.val <= min) return false;
+
+    return helper(node.left, node.val, min) && helper(node.right, max, node.val);
+
+};
