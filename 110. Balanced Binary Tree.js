d716c268775a3ae0543e30c0d1ae73112a9ca59e
KUO <drjkuo@gmail.com>
2018-01-25 20:22:35 -0800

    O(nlogn)

--- 110. Balanced Binary Tree.js
+++ 110. Balanced Binary Tree.js
@@ -25,3 +25,26 @@ var nodeHeight = function(node) {
     if (node === null) return 0;
     return Math.max(nodeHeight(node.left), nodeHeight(node.right)) + 1;
 }
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function(root) {
+    if (root === null) return true;
+
+    if (Math.abs(getHeight(root.left) - getHeight(root.right)) > 1) return false;
+    return isBalanced(root.left) && isBalanced(root.right);
+};
+
+function getHeight(root) {
+    if (root === null) return 0;
+    return Math.max(getHeight(root.left), getHeight(root.right)) + 1;
+}
