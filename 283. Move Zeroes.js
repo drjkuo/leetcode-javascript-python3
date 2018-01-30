0817d5eede3e662303ce928a7a03453d64837f08
KUO <drjkuo@gmail.com>
2018-01-30 02:20:17 -0800

    need refinement

---
+++ 283. Move Zeroes.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    var len = nums.length;
+    for (var i=0; i<len; i++) {
+        if (nums[i] === 0) {
+            nums.splice(i,1);
+            nums.push(0);
+            i--;
+            len--;
+        }
+    }
+    return;
+};
