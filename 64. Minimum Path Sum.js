8c787a607ba58f60511005d715688fcc52f925c9
KUO <drjkuo@gmail.com>
2018-02-03 11:45:32 -0800

    dp; OK

--- 64. Minimum Path Sum.js
+++ 64. Minimum Path Sum.js
@@ -34,3 +34,33 @@ var minPathSum = function(grid) {
     }
 
 };
+
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var minPathSum = function(grid) {
+    var rowlen = grid.length;
+    var collen = grid[0].length;
+    var dp = [];
+    for (var i=0; i<rowlen; i++) {
+        dp.push([]);
+        for (var j=0; j<collen; j++) {
+            if (i === 0 && j === 0) {
+                dp[i][j] = grid[i][j];
+                continue;
+            }
+            if (i === 0) {
+                dp[i][j] = dp[i][j-1] + grid[i][j];
+                continue;
+            }
+
+            if (j === 0) {
+                dp[i][j] =  dp[i-1][j] + grid[i][j];
+                continue;
+            }
+            dp[i][j] = Math.min(dp[i-1][j], dp[i][j-1]) + grid[i][j];
+        }
+    }
+    return dp[i-1][j-1];
+};
