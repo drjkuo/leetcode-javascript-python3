d502b0bee1b1cee205a1d7da5d72bfba82e7bf8e
KUO <drjkuo@gmail.com>
2018-02-07 01:37:45 -0800

    ok

--- 235. Lowest Common Ancestor of a Binary Search Tree.js
+++ 235. Lowest Common Ancestor of a Binary Search Tree.js
@@ -26,5 +26,32 @@ var lowestCommonAncestor = function(root, p, q) {
             return root;
         }
     }
+};
 
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+    while ((root.val >= p.val && root.val >= q.val) || (root.val <= p.val && root.val <= q.val)) {
+        if (root.val === p.val || root.val === q.val)
+            return root;
+        if (root.val > p.val && root.val > q.val)
+            root = root.left;
+        if (root.val < p.val && root.val < q.val)
+            root = root.right;
+    }
+
+    return root;
 };
