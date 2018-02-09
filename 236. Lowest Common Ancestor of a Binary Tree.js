f34c7249bd2b9cd70a3628608ce9a480a621e20c
KUO <drjkuo@gmail.com>
2018-02-08 23:37:23 -0800

    ok

--- 236. Lowest Common Ancestor of a Binary Tree.js
+++ 236. Lowest Common Ancestor of a Binary Tree.js
@@ -40,3 +40,32 @@ var lowestCommonAncestor = function(root, p, q) {
 // if p === root or q === root return root
 // if p,q on diffrent child tree return root
 // if p,q is not in the tree return null
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+    if (root === null || root === p || root === q) return root;
+    var left = lowestCommonAncestor(root.left, p, q);
+    var right = lowestCommonAncestor(root.right, p, q);
+    if (left !== null && right !==null) return root;
+    return (left)? left : right;
+    // if p in right && q in right, root = root.right
+    // if p in right && q in left, root = root.left
+    // if only p return p, if only q return q
+    // if p, q are on different subtree return root
+    // root === p || root === q return root;
+
+
+};
