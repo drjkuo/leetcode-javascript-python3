ea846547b661948fbdc046fc684df138d5588ef6
KUO <drjkuo@gmail.com>
2018-02-09 02:44:27 -0800

    refine

--- 100. Same Tree.js
+++ 100. Same Tree.js
@@ -19,3 +19,23 @@ var isSameTree = function(p, q) {
     return true;
 
 };
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+    if (p === null && q === null) return true;
+    if (p === null || q === null) return false;
+    if (p.val !== q.val) return false;
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
