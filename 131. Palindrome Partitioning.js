c644b6ebcc4dd344140ccfcc15ae9411af1f7cfd
KUO <drjkuo@gmail.com>
2018-02-08 23:37:30 -0800

    ok

--- 131. Palindrome Partitioning.js
+++ 131. Palindrome Partitioning.js
@@ -33,3 +33,44 @@ var partition = function(s) {
        return true;
     }
 };
+
+/**
+ * @param {string} s
+ * @return {string[][]}
+ */
+var partition = function(s) {
+    var result = [];
+    var oneAns = [];
+    helper(result, oneAns, s, 0);
+    return result;
+};
+
+
+function helper(result, oneAns, s, pos) {
+    var len = s.length;
+    if (pos === len) {
+        result.push(oneAns.slice());
+        return;
+    }
+
+    for (var j=pos+1; j<=len; j++) {
+        var subs = s.substring(pos, j);
+        if (isPalindrome(subs)) {
+            oneAns.push(subs);
+            helper(result, oneAns, s, j);
+            oneAns.pop();
+        }
+    }
+}
+
+function isPalindrome(s) {
+    if (s.length === 0) return false;
+    var start = 0, end = s.length-1;
+    while (start < end) {
+        if (s[start] !== s[end])
+            return false;
+        start += 1;
+        end -= 1;
+    }
+    return true;
+}
