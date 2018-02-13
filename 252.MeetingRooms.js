e79735c489637b3dc4e7751aba36738bd16d7538
KUO <drjkuo@gmail.com>
2018-02-13 02:22:49 -0800

    if x>y return 1 ascending

--- 252.MeetingRooms.js
+++ 252.MeetingRooms.js
@@ -31,3 +31,29 @@ var canAttendMeetings = function(intervals) {
 
     return true;
 };
+
+
+/**
+ * Definition for an interval.
+ * function Interval(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ * }
+ */
+/**
+ * @param {Interval[]} intervals
+ * @return {boolean}
+ */
+var canAttendMeetings = function(intervals) {
+    if (intervals.length <= 1) return true;
+    intervals.sort(function(x,y) {
+        if (x.start>y.start) return 1;
+        else return -1;
+    });
+
+    for (var i=1; i<intervals.length; i++) {
+        if (intervals[i].start<intervals[i-1].end) return false;
+    }
+
+    return true;
+};
