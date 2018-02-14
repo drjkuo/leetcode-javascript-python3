01e5b79a181d383774ccedd267c8a58fb09e347b
KUO <drjkuo@gmail.com>
2018-02-14 11:35:38 -0800

    2nd

--- 53. Maximum Subarray.js
+++ 53. Maximum Subarray.js
@@ -14,3 +14,23 @@ var maxSubArray = function(nums) {
     }
     return ans;
 };
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    if (nums.length === 0) return 0;
+    if (nums.length === 1) return nums[0];
+
+    var result = -Infinity;
+    var max = [];
+    max[0] = nums[0];
+    result = Math.max(max[0], result);
+    for (var i=1; i<nums.length; i++) {
+        max[i] = Math.max(max[i-1]+nums[i], nums[i]);
+        result = Math.max(max[i], result);
+    }
+
+    return result;
+};
