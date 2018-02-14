bcec4ac49b03d273bc8409a310f74396cdf927f6
KUO <drjkuo@gmail.com>
2018-02-13 23:55:54 -0800

    mine

--- 270. Closest Binary Search Tree Value.js
+++ 270. Closest Binary Search Tree Value.js
@@ -25,5 +25,31 @@ var closestValue = function(root, target) {
 
 
 
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} target
+ * @return {number}
+ */
+var closestValue = function(root, target) {
+    if (root === null) return;
 
+    var diff = Infinity, result = Infinity;
+    while (root) {
+        if (Math.abs(target-root.val) < diff) {
+            result = root.val;
+            diff = Math.abs(target-root.val);
+        }
 
+        if (root.val > target) root = root.left;
+        else root = root.right;
+    }
+
+    return result;
+};
