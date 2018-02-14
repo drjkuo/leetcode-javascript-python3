769cf7b88d95b8247df792c24785d3733ad34e85
KUO <drjkuo@gmail.com>
2018-02-14 01:25:01 -0800

    ok

--- 198. House Robber.js
+++ 198. House Robber.js
@@ -18,3 +18,24 @@ var rob = function(nums) {
 
     return max.pop();
 };
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+    if (nums.length === 0) return 0;
+    if (nums.length === 1) return nums[0];
+    if (nums.length === 2) return Math.max(nums[0], nums[1]);
+
+    var sum = [];
+    sum[0] = nums[0];
+    sum[1] = Math.max(nums[0], nums[1]);
+
+    for (var i=2; i<nums.length; i++) {
+        sum[i] = Math.max(sum[i-1], sum[i-2]+nums[i]);
+    }
+
+    return sum[nums.length-1];
+};
