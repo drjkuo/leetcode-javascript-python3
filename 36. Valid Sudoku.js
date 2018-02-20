df11ae2c04585e69ad6f9cbb20b1f3db322e1b6f
KUO <drjkuo@gmail.com>
2018-02-19 20:38:01 -0800

    2nd

--- 36. Valid Sudoku.js
+++ 36. Valid Sudoku.js
@@ -28,3 +28,53 @@ var isValidSudoku = function(board) {
     return true;
 
 };
+
+
+
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+var isValidSudoku = function(board) {
+    var hashRow = {};
+    var hashCol = {};
+    var hashBlock = {};
+    var rowLen = board.length;
+    var colLen = board[0].length;
+    // check row
+    // check col
+    // check block
+
+    for (var i=0; i<rowLen; i++) {
+        for (var j=0; j<colLen; j++) {
+            if (board[i][j] !== "." && hashRow[parseInt(board[i][j])] !== undefined) return false;
+            else hashRow[parseInt(board[i][j])] = true;
+        }
+        hashRow = {};
+    }
+
+    for (var i=0; i<rowLen; i++) {
+        for (var j=0; j<colLen; j++) {
+            if (board[j][i] !== "." && hashCol[parseInt(board[j][i])] !== undefined) return false;
+            else hashCol[parseInt(board[j][i])] = true;
+        }
+        hashCol = {};
+    }
+
+    for (var i=0; i<rowLen; i++) {
+        for (var j=0; j<colLen; j++) {
+            var rowIndex = 3*parseInt(i/3); // 0,0,0,3...,6..
+            var colIndex = 3*(i%3); // 0,1,2,0,1,2
+            rowIndex = rowIndex + parseInt(j/3);
+            colIndex = colIndex + j%3;
+
+            if (board[rowIndex][colIndex] !== "." && hashBlock[parseInt(board[rowIndex][colIndex])] !== undefined) return false;
+            else hashBlock[parseInt(board[rowIndex][colIndex])] = true;
+        }
+        hashBlock = {};
+    }
+
+    return true;
+
+
+};
