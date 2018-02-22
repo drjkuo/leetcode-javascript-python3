617360855ec3424f7813b912d2a4978a0c814b33
KUO <drjkuo@gmail.com>
2018-02-22 04:08:24 -0800

    ok

--- 380. Insert Delete GetRandom O(1).js
+++ 380. Insert Delete GetRandom O(1).js
@@ -66,3 +66,62 @@ RandomizedSet.prototype.getRandom = function() {
  * var param_2 = obj.remove(val)
  * var param_3 = obj.getRandom()
  */
+
+
+ /**
+  * Initialize your data structure here.
+  */
+ var RandomizedSet = function() {
+     this.hash = {};
+     this.array = [];
+ };
+
+ /**
+  * Inserts a value to the set. Returns true if the set did not already contain the specified element.
+  * @param {number} val
+  * @return {boolean}
+  */
+ RandomizedSet.prototype.insert = function(val) {
+     var hash = this.hash;
+     var array = this.array;
+     if (hash[val] !== undefined) return false;
+     array.push(val);
+     hash[val] = array.length-1;
+     return true;
+ };
+
+ /**
+  * Removes a value from the set. Returns true if the set contained the specified element.
+  * @param {number} val
+  * @return {boolean}
+  */
+ RandomizedSet.prototype.remove = function(val) {
+     var hash = this.hash;
+     var array = this.array;
+     if (hash[val] === undefined) return false;
+     var lastValue = array[array.length-1];
+     var deletingIndex = hash[val];
+     hash[lastValue] = deletingIndex;
+     array[deletingIndex] = lastValue;
+     delete hash[val];
+     array.pop();
+     return true;
+ };
+
+ /**
+  * Get a random element from the set.
+  * @return {number}
+  */
+ RandomizedSet.prototype.getRandom = function() {
+     var array = this.array;
+     var randomIndex = Math.floor(Math.random()*array.length);
+     return array[randomIndex];
+ };
+
+ /**
+  * Your RandomizedSet object will be instantiated and called as such:
+  * var obj = Object.create(RandomizedSet).createNew()
+  * var param_1 = obj.insert(val)
+  * var param_2 = obj.remove(val)
+  * var param_3 = obj.getRandom()
+  */
