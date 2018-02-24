3209a5c686272b8e447d4ce3d87415f63f0eef4b
KUO <drjkuo@gmail.com>
2018-02-24 11:44:31 -0800

    ok

--- 412.FizzBuzz.js
+++ 412.FizzBuzz.js
@@ -31,3 +31,35 @@ var fizzBuzz = function(n) {
     return str;
 
 };
+
+
+
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+    if (typeof n !== "number") throw new Error("input error");
+
+    var result = [];
+    for (var i=1; i<=n; i++) {
+        if (i%3 === 0 && i%5 === 0) {
+            result.push("FizzBuzz");
+            continue;
+        }
+        else if (i%3 === 0) {
+            result.push("Fizz");
+            continue;
+        }
+        else if (i%5 === 0) {
+            result.push("Buzz");
+            continue;
+        }
+        else {
+            result.push(i.toString());
+        }
+    }
+
+    return result;
+
+};
