b53d6c76ef53b541fc77cef689ca0df34adb44fa
KUO <drjkuo@gmail.com>
2018-02-23 22:39:17 -0800

    faster

--- 14. Longest Common Prefix.js
+++ 14. Longest Common Prefix.js
@@ -28,3 +28,25 @@ var longestCommonPrefix = function(strs) {
     return minS.substring(0, i);
 
 };
+
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    if (strs instanceof Array === false) throw new Error("input error");
+    if (strs.length === 0) return "";
+    if (strs.length === 1) return strs[0];
+
+    for (var i=0; i<strs[0].length; i++) {
+        for (var j=0; j<strs.length-1; j++) {
+            if (i>=strs[j].length || i>=strs[j+1].length || strs[j][i] !== strs[j+1][i])
+                return strs[0].substring(0, i);
+        }
+    }
+
+    return strs[0];
+
+
+};
