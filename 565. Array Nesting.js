6bc5522d8c121d931249e20c03a2a21c2bdde4aa
KUO <drjkuo@gmail.com>
2018-02-24 23:48:13 -0800

    DFS; time limit

---
+++ 565. Array Nesting.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arrayNesting = function(nums) {
+    if (nums === null) throw new Error("input error");
+    if (nums.length === 0) return 0;
+
+    var visited = [], result = -Infinity;
+
+    for (var i=0; i<nums.length; i++) {
+        visited = [];
+        DFS(nums, visited, i, 0);
+    }
+
+    return result;
+
+
+    function DFS(nums, visited, i, lengthS) {
+        if (visited.indexOf(i) !== -1) { // visited
+            result = Math.max(result, lengthS);
+            return;
+        }
+        else { // not visited
+            visited.push(i);
+            DFS(nums, visited, nums[i], lengthS+1);
+            visited.pop();
+        }
+    }
+};
