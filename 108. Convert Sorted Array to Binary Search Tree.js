d3db437554631f0cb2b0f682adc5d1cdb302ae0f
KUO <drjkuo@gmail.com>
2018-02-25 12:55:01 -0800

    ok

--- 108. Convert Sorted Array to Binary Search Tree.js
+++ 108. Convert Sorted Array to Binary Search Tree.js
@@ -24,3 +24,31 @@ var helper = function (arr, start, end) {
 
     return node;
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var sortedArrayToBST = function(nums) {
+    if (nums.length === 0 || nums === null) return null;
+
+    return helper(nums, 0, nums.length-1);
+};
+
+function helper(nums, lindex, rindex) {
+    if (lindex > rindex) return null;
+    var mid = Math.ceil((lindex+rindex)/2);
+    var root = new TreeNode(nums[mid]);
+    var left = helper(nums, lindex, mid-1);
+    var right = helper(nums, mid+1, rindex);
+    root.left = left;
+    root.right = right;
+    return root;
+}
