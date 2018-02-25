1ffa28a17739f06718d4aeef22ba97cbc4182410
KUO <drjkuo@gmail.com>
2018-02-24 16:57:02 -0800

    ok

--- 88. Merge Sorted Array.js
+++ 88. Merge Sorted Array.js
@@ -47,3 +47,49 @@ var merge = function(nums1, m, nums2, n) {
 
 
 };
+
+
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    if (nums1 instanceof Array === false || nums2 instanceof Array === false) throw new Error("input error");
+
+    var i=m-1, j=n-1, p=n+m-1;
+    while (i>=0 || j>=0) {
+        if (i>=0 && j>=0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[p] = nums1[i];
+                i -= 1;
+                p -= 1;
+            }
+            else {
+                nums1[p] = nums2[j];
+                j -= 1;
+                p -= 1;
+            }
+            continue;
+        }
+
+        if (j>=0) {
+            nums1[p] = nums2[j];
+            j -= 1;
+            p -= 1;
+            continue;
+        }
+
+        if (i>=0) {
+            nums1[p] = nums1[i];
+            i -= 1;
+            p -= 1;
+            continue;
+        }
+    }
+
+    return;
+
+};
