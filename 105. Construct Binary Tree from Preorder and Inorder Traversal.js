8fdd8474cad9686d5ce3809c8ff6bdbb29082a0a
KUO <drjkuo@gmail.com>
2018-02-26 18:50:37 -0800

    root.left; root.right; root

--- 105. Construct Binary Tree from Preorder and Inorder Traversal.js
+++ 105. Construct Binary Tree from Preorder and Inorder Traversal.js
@@ -47,3 +47,37 @@ function helper(lp,rp,arrp,li,ri,arri) {
     return node;
 
 }
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+var buildTree = function(preorder, inorder) {
+    if (preorder === null || preorder.length === 0) return null;
+    return helper(preorder, 0, preorder.length-1, inorder, 0, inorder.length-1);
+
+    function helper(preorder, pl, pr, inorder, il, ir) {
+        if (pl > pr) return null;
+
+        var root = new TreeNode(preorder[pl]);
+
+        for (var rootpos=il; rootpos<=ir; rootpos++) {
+            if (inorder[rootpos] === preorder[pl])
+                break;
+        }
+
+        root.left = helper(preorder, pl+1, rootpos-il+pl, inorder, il, rootpos-1);
+        root.right = helper(preorder, rootpos-il+pl+1, pr, inorder, rootpos+1, ir);
+
+        return root;
+    }
+};
