c4ce16fbbd8807dfb1358ca034b3d536123ec40b
KUO <drjkuo@gmail.com>
2018-03-01 21:33:27 -0800

    DFS

--- 386. Lexicographical Numbers.js
+++ 386. Lexicographical Numbers.js
@@ -15,3 +15,23 @@ var lexicalOrder = function(n) {
 
     return result;
 };
+
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var lexicalOrder = function(n) {
+    var result = [];
+    helper(result, 0, n);
+    return result;
+
+    function helper(result, x, n) {
+        if (x > n) return;
+        if (x <= n && x!==0) result.push(x);
+
+        for (var i=0; i<=9; i++) {
+            if (x===0 && i===0) continue;
+            helper(result, x*10+i, n);
+        }
+    }
+};
