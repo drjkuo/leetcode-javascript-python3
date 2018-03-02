3fd699def7c56872afa469357153f4e13e088d18
KUO <drjkuo@gmail.com>
2018-03-02 02:09:51 -0800

    neat

--- 112.PathSum.js
+++ 112.PathSum.js
@@ -22,3 +22,22 @@ let helper = function(node, remaining) // make sure of end condition
     if (node.val === remaining && node.left === null && node.right === null) return true; // true condition: node.val && leaf
     return helper(node.left, remaining - node.val) || helper(node.right, remaining - node.val);
 }
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {boolean}
+ */
+var hasPathSum = function(root, sum) {
+    if (root === null) return false;
+    if (root.val === sum && root.left === null && root.right === null) return true;
+
+    return (hasPathSum(root.left, sum-root.val) || hasPathSum(root.right, sum-root.val));
+};
